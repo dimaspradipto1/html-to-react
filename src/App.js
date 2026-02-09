76c735a24557627704633ca3053435311b8659c7
dimas pradipto <dpradipto8@gmail.com>
2026-02-09 15:43:17 +0700

    feat: feature section

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from 'react'
 import "assets/css/style.css"
 import Homepage from "scenes/Homepage"
-import Header from "parts/Header"
-import Clients from "parts/Clients"
 
 function App() {
   return (
-    <>
-    <Header />
-    <Homepage />
-    <Clients />
-    </>
+   <div className="App">
+      <Homepage /> 
+    </div>
   );
 }
 
--- src/scenes/Homepage.js
+++ src/scenes/Homepage.js
@@ -1,11 +1,42 @@
 import React from "react";
 import { Fade } from "react-awesome-reveal";
 import Section from "elements/Section";
+import Header from "parts/Header";
+import Clients from "parts/Clients";
+import Feature from "parts/Feature";
+
 import HeroImage from "assets/images/hero-image.png";
+import FeatureTile01 from "assets/images/feature-tile-icon-01.svg";
+import FeatureTile02 from "assets/images/feature-tile-icon-02.svg";
+import FeatureTile03 from "assets/images/feature-tile-icon-03.svg";
 
 export default function Homepage() {
+  const feature = [
+    {
+      imgSrc: FeatureTile01,
+      imgAlt: "Feature tile icon 01",
+      title: "Join the system",
+      description:
+        "A pseudo-Latin text used in web design, layout, and printing in place of things to emphasise design.",
+    },
+    {
+      imgSrc: FeatureTile02,
+      imgAlt: "Feature tile icon 02",
+      title: "Join the system",
+      description:
+        "A pseudo-Latin text used in web design, layout, and printing in place of things to emphasise design.",
+    },
+    {
+      imgSrc: FeatureTile03,
+      imgAlt: "Feature tile icon 03",
+      title: "Join the system",
+      description:
+        "A pseudo-Latin text used in web design, layout, and printing in place of things to emphasise design.",
+    },
+  ];
   return (
     <div className="body-wrap">
+      <Header />
       <main className="site-content">
         <Section className="hero illustration-section-01" isCenteredContent>
           <div className="container-sm">
@@ -36,6 +67,18 @@ export default function Homepage() {
                   />
                 </div>
               </Fade>
+            </div>
+          </div>
+        </Section>
+        <Clients />
+        <Section className="features-tiles">
+          <div className="container">
+            <div className="features-tiles-inner section-inner">
+              <div className="tiles-wrap">
+                {feature.map((item, index) => (
+                  <Feature key={index} delayInMS={index * 500} data={item} />
+                ))}
+              </div>
             </div>
           </div>
         </Section>
